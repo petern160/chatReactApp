afe4368795dd1365ff90192e61a709ab891856a8
petern160 <peternguyen160@gmail.com>
2019-05-09 22:13:27 +0000

    removed some console logs

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ const URL = "ws://localhost:3001";
 const ws = new WebSocket(URL);
 
 const letters = `0123456789ABCDEF`;
-   let userColor = `#`;
-   for (let i = 0; i < 6; i++) {
-     userColor += letters[Math.floor(Math.random() * 16)];
-   }
+let userColor = `#`;
+for (let i = 0; i < 6; i++) {
+  userColor += letters[Math.floor(Math.random() * 16)];
+}
 
 class App extends Component {
   constructor(props) {
@@ -21,58 +21,50 @@ class App extends Component {
       currentUser: { name: "Anonymous" }, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [],
       clientNumber: 0,
-      color:userColor
+      color: userColor
     };
-    this.updateUsername = this.updateUsername.bind(this)
+    this.updateUsername = this.updateUsername.bind(this);
     this.addMessage = this.addMessage.bind(this);
   }
-  
-  
+
   componentDidMount() {
     ws.onopen = () => {
       console.log("connected");
     };
 
-
-    // receives number of users online and and messages from server 
+    // receives number of users online and and messages from server
     ws.onmessage = evt => {
-      const loggedUsers = JSON.parse(evt.data)
-      console.log(`this is clients logged info: `,loggedUsers)
-      if(loggedUsers.clientNumber){
-        this.setState ({clientNumber: loggedUsers.clientNumber})
+      const loggedUsers = JSON.parse(evt.data);
+      console.log(`this is clients logged info: `, loggedUsers);
+      if (loggedUsers.clientNumber) {
+        this.setState({ clientNumber: loggedUsers.clientNumber });
         return;
       }
       // adds message to page
-      const message = JSON.parse(evt.data)
-      console.log(message)
-      this.addToPage(message)
-    }
+      const message = JSON.parse(evt.data);
+      console.log(message);
+      this.addToPage(message);
+    };
   }
 
   // changes state of username
-  updateUsername ({username}){
-      console.log('will udate to this username!', username);
-      this.setState ({currentUser: { name: username}})
+  updateUsername({ username }) {
+    this.setState({ currentUser: { name: username } });
   }
 
   // renders message on page
-  addToPage(message){
+  addToPage(message) {
     const oldMessage = this.state.messages;
     const newMessage = [...oldMessage, message];
     this.setState({ messages: newMessage });
   }
 
-
   // sends messages to server
   addMessage(message) {
-
-    
     ws.send(JSON.stringify(message));
   }
 
-  
   render() {
-    
     return (
       // different components with props passed down
       <div className>
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import App from "./App.jsx";
 
-
 const letters = `0123456789ABCDEF`;
-   let userColor = `#`;
-   for (let i = 0; i < 6; i++) {
-     userColor += letters[Math.floor(Math.random() * 16)];
-   }
-
+let userColor = `#`;
+for (let i = 0; i < 6; i++) {
+  userColor += letters[Math.floor(Math.random() * 16)];
+}
 
 class Chatbar extends Component {
   constructor(props) {
@@ -16,13 +14,12 @@ class Chatbar extends Component {
 
   render() {
     // if username is blank becomes Anonymous
-    let { currentUser: { name }} = this.props;
-    let userName =
-    name && name.length !== ""
-      ? name
-      : 'Anonymous';
+    let {
+      currentUser: { name }
+    } = this.props;
+    let userName = name && name.length !== "" ? name : "Anonymous";
 
-      // on enter sends data to server
+    // on enter sends data to server
     const keyPressEnter = event => {
       if (event.key === "Enter") {
         const obj = {
@@ -39,20 +36,21 @@ class Chatbar extends Component {
 
     // on enter updates username and sends username to server
     const enterKeyUser = event => {
-      if(event.key ==="Enter"){
+      if (event.key === "Enter") {
         // send obj to server
-      const obj = {
-        type: 'UserNameChange',
-        content: `${this.props.currentUser.name} has changed name to ${event.target.value}`,
-        olduser: this.props.currentUser.name,
-        username: event.target.value
+        const obj = {
+          type: "UserNameChange",
+          content: `${this.props.currentUser.name} has changed name to ${
+            event.target.value
+          }`,
+          olduser: this.props.currentUser.name,
+          username: event.target.value
+        };
+        this.props.updateUsername(obj);
+        event.target.value = "";
+        this.props.addMessage(obj);
       }
-       this.props.updateUsername(obj)
-       event.target.value = ""
-       this.props.addMessage(obj)
-      }
-    }
-    
+    };
 
     return (
       <div>
